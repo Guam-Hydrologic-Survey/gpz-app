c5ca90fd980ce37d78baab37fae1fa3dd8fd0b74
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-20 19:03:44 +1000

    rm v2 in import statements

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ Description: Entry point for project. Appends all components to #app in index.ht
 */
 
 // components 
-import { About } from "./components/About_v2.js";
-import { NavBar } from "./components/NavBar_v2.js";
-import { Legend } from "./components/Legend_v2.js";
+import { About } from "./components/About.js";
+import { NavBar } from "./components/NavBar.js";
+import { Legend } from "./components/Legend.js";
 import { MapContainer } from "./components/MapContainer.js";
 import { LMap } from "./components/LMap.js";
 
