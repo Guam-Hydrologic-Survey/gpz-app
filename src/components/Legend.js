b1267dd3ee838248a73293e5c90bd018313f7110
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-20 19:42:05 +1000

    add legend body contents
    - svg swatches of polygon colors
    - names of maps with links to pdfs
    
    *note: all are hardcoded (i.e., not imported using the constant)

--- src/components/Legend.js
+++ src/components/Legend.js
@@ -22,12 +22,46 @@ export function Legend() {
     <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
     `;
 
+    const HEIGHT = 50;
+    const WIDTH = 50;
+
     // legend contents 
     const contents = document.createElement("div");
     contents.className = "offcanvas-body";
     contents.id = "legend-contents";
 
     contents.innerHTML = /*html*/ `
+    <hr>
+    <svg width="${WIDTH}" height="${HEIGHT}" xmlns="http://www.w3.org/2000/svg">
+        <rect 
+            width="${WIDTH}" 
+            height="${HEIGHT}" 
+            fill="#ffea00" 
+            rx="5"
+            ></rect>
+    </svg>
+    <a 
+        href="https://ghs-cdn.uog.edu/wp-content/databases/MAppFx/gpz-app/CDM82%20-%20Fig8%20GWPZ.pdf" 
+        target="_blank" 
+        rel="noreferrer noopener" 
+        title="GPZ CDM 1982"
+        >GPZ CDM 1982</a>
+    <hr>
+    <svg width="${WIDTH}" height="${HEIGHT}" xmlns="http://www.w3.org/2000/svg">
+        <rect 
+            width="${WIDTH}" 
+            height="${HEIGHT}" 
+            fill="#c77dff" 
+            rx="5"
+            ></rect>
+    </svg>
+    <a 
+        href="https://ghs-cdn.uog.edu/wp-content/databases/MAppFx/gpz-app/1997-DLM-GPZ-Overlay-Map.pdf" 
+        target="_blank" 
+        rel="noreferrer noopener" 
+        title="GPZ DLM 1997"
+        >GPZ DLM 1997</a>
+    <hr>
     `;
 
     legend.append(header, contents);
