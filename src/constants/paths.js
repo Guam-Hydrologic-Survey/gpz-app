97f95a4fc6ac523258373774a7bb2dc5fb05f621
Dannika <71364766+dannikate@users.noreply.github.com>
2026-09-20 17:59:50 +1000

    create paths
    contains an array list of objects that contain the following properties:
    - name: string containing the map name
    - path: string containing the path of the json file
    - pdf: string containing the link to the PDF of the reference map
    - color: string containing the hex value of the map feature (polygon)

---
+++ src/constants/paths.js
@@ -0,0 +1,32 @@
+/* 
+paths.js
+Description: Paths to JSON files 
+*/
+
+export const gpz_maps = [
+    {
+        map: {
+            name: "GPZ CDM 1982",
+            path: "./src/data/GPZCDM1982.json",
+            pdf: "https://ghs-cdn.uog.edu/wp-content/databases/MAppFx/gpz-app/CDM82%20-%20Fig8%20GWPZ.pdf",
+            color: "#ffea00"
+        }
+    },
+    {
+        map: {
+            name: "GPZ DLM 1997",
+            path: "./src/data/GPZDLM1997.json",
+            pdf: "https://ghs-cdn.uog.edu/wp-content/databases/MAppFx/gpz-app/1997-DLM-GPZ-Overlay-Map.pdf",
+            color: "#c77dff" // orange 
+        }
+    }
+]
+
+/*
+COLORS: 
+----
+yellow: #ffea00
+orange: #ff7b00
+purple: #c77dff
+*/
+
